2d72ffe4a16a772ffd699f57dff59e0fe4e8e57c
Thales Burque <thalesburque@gmail.com>
2022-04-19 18:04:30 -0300

    adiciona flyway

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,78 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.algaworks.algafood.domain.model.enums.StatusPedido;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Pedido {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private BigDecimal subtotal;
+
+	@Column(nullable = false)
+	private BigDecimal taxaFrete;
+
+	@Column(nullable = false)
+	private BigDecimal valorTotal;
+
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCriacao;
+
+	@Column(columnDefinition = "datetime")
+	private LocalDateTime dataConfirmacao;
+
+	@Column(columnDefinition = "datetime")
+	private LocalDateTime dataCancelamento;
+
+	@Column(columnDefinition = "datetime")
+	private LocalDateTime dataEntrega;
+
+	@Column(nullable = false)
+	private StatusPedido status;
+
+	@Embedded
+	private Endereco enderecoEntrega;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Usuario cliente;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private FormaPagamento formaPagamento;
+
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
+
+}
--- algafood-api/src/main/resources/db/migration/V003__cria-tabela-estado-altera-tabela-cidade.sql
+++ algafood-api/src/main/resources/db/migration/V003__cria-tabela-estado-altera-tabela-cidade.sql
@@ -11,7 +11,7 @@ alter table cidade add column estado_id bigint not null;
 
 update cidade c set c.estado_id = (select e.id from estado e where e.nome = c.nome_estado);
 
-alter table cidade add constraint fk_cidade_estado
+alter table cidade add constraint fk_cidade_estado 
 foreign key (estado_id) references estado(id);
 
 alter table cidade drop column nome_estado;
